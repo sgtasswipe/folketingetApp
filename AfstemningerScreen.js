9d437f155b982b9b02d8eae4e629744fe054692b
sgtasswipe <otro0001@stud.kea.dk>
2025-03-28 15:46:32 +0100

    updated navigation. User is redirected after login

---
+++ AfstemningerScreen.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View, TextInput, Pressable, Text } from "react-native";
+
+export default function AfstemningerScreen() {
+
+    return( 
+        <View> 
+<Text>Hello from Afstemninger Screen</Text>
+        </View>
+    )
+}
