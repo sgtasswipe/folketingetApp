d396385518bd1a0f4ad5c6694455e3efc5a43dd3
Hasan Rashid <hara0001@stud.kea.dk>
2025-04-07 19:51:05 +0200

    Moved VotingCard functionality to seperate file

--- AfstemningerScreen.js
+++ AfstemningerScreen.js
@@ -3,13 +3,10 @@ import {
   View, 
   Text, 
   FlatList, 
-  StyleSheet, 
   ActivityIndicator,
-  TouchableOpacity 
 } from 'react-native';
-import { format } from 'date-fns';
-import { da } from 'date-fns/locale';
 import { styles } from "./styles/AfstemningerScreenStyles";
+import VotingCard from './VotingCard';  // Assuming VotingCard is in the same directory
 
 
 const AfstemningerScreen = () => {
@@ -58,57 +55,10 @@ const AfstemningerScreen = () => {
     }
   };
 
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return format(date, 'd. MMMM yyyy', { locale: da });
-  };
-
-
-
+ 
   
   const renderVotingCard = ({ item }) => {
-    return (
-      <TouchableOpacity 
-      onPress={() => setSelected(item.id === selected ? null : item.id)}
-        style={[
-          styles.card,
-          { borderLeftColor: item.vedtaget ? '#4CAF50' : '#F44336' }
-        ]}
-      >
-      <View style={[styles.cardHeader, { flexDirection: 'column', alignItems: 'flex-start' }]}>
-       <Text style={styles.cardTitle}>
-        {item.Sagstrin.Sag.titel}
-        </Text>
-
-       
-      </View>
-      
-        <Text style={styles.dateText}>
-          {formatDate(item.opdateringsdato)}
-        </Text>
-
-        <Text style={[styles.statusBadge, { backgroundColor: item.vedtaget ? '#E8F5E9' : '#FFEBEE' }]}>
-        {item.vedtaget ? 'Vedtaget' : 'Ikke vedtaget'}
-        </Text>
-        
-        {selected ==item.id ? ( 
-            <Text>{item.konklusion}</Text>
-        ) :  (
-        selected==item.id, item.konklusion &&    (
-        
-          <Text style={styles.conclusionText} numberOfLines={3}>
-            {item.konklusion.replace(/\\n/g, '\n')}
-          </Text>
-        )
-        )}
-
-        
-        <View style={styles.cardFooter}>
-          <Text style={styles.meetingText}>Møde ID: {item.mødeid}</Text>
-          <Text style={styles.typeText}>Type: {item.typeid}</Text>
-        </View>
-      </TouchableOpacity>
-    );
+    return <VotingCard item={item} selected={selected} setSelected={setSelected} />;
   };
 
   const renderFooter = () => {
