2527f401fe9caef7e9e024ad65e40b5eae65e13e
Hasan Rashid <hara0001@stud.kea.dk>
2025-04-16 20:57:06 +0200

    Moved sensitive info to env file

---
+++ app.config.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';  // Importér dotenv for at læse .env filen
+
+export default {
+  expo: {
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+  },
+};
